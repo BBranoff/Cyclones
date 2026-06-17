ddba85616df35d9ede54c01ce4bf5d3d964ee1bb
BenBranoff <benjamin.branoff@usda.gov>
2026-06-17 13:53:12 -0400

    starting build site process over

--- docs/tests/testthat/test-build_models.R
+++
@@ -1,8 +0,0 @@
-allstorms <- get_storms("E:/OneDrive - USDA/Hurricanes/ibtracs.ALL.list.v04r01.csv")
-fewstorms <-  get_storms(ib_filt="last3years")
-test_that("build_models works", {
-  expect_no_error(build_models(allstorms))
-})
-test_that("too few build_models works", {
-  expect_no_error(build_models(fewstorms))
-})
